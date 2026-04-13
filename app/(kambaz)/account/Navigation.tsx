4ffc202844121e8d404dfa91339526d269c7f185
dsilva-rhianna <60183173+dsilva-rhianna@users.noreply.github.com>
2026-04-13 18:55:40 -0400

    a6

--- app/(kambaz)/account/Navigation.tsx
+++ app/(kambaz)/account/Navigation.tsx
@@ -4,16 +4,42 @@ import { usePathname } from "next/navigation";
 import { Nav, NavItem, NavLink } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
+
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: RootState) => state.accountReducer);
-  const links = currentUser ? ["profile"] : ["signin", "signup"];
- const pathname = usePathname();
- return (
-   <Nav variant="pills">
-     {links.map((link) => (
-       <NavItem key={link}>
-         <NavLink as={Link} href={link} active={pathname.endsWith(link)}>
-           {link} </NavLink> </NavItem>
-     ))}
-   </Nav>
-);}
+  const pathname = usePathname();
+
+  return (
+    <Nav variant="pills">
+      {currentUser ? (
+        <>
+          <NavItem>
+            <NavLink as={Link} href="/account/profile" active={pathname.endsWith("profile")}>
+              Profile
+            </NavLink>
+          </NavItem>
+          {currentUser.role === "ADMIN" && (
+            <NavItem>
+              <NavLink as={Link} href="/account/users" active={pathname.endsWith("users")}>
+                Users
+              </NavLink>
+            </NavItem>
+          )}
+        </>
+      ) : (
+        <>
+          <NavItem>
+            <NavLink as={Link} href="/account/signin" active={pathname.endsWith("signin")}>
+              signin
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink as={Link} href="/account/signup" active={pathname.endsWith("signup")}>
+              signup
+            </NavLink>
+          </NavItem>
+        </>
+      )}
+    </Nav>
+  );
+}
---
+++ app/(kambaz)/courses/[cid]/people/table.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Table } from "react-bootstrap";
+import { FaUserCircle } from "react-icons/fa";
+import * as client from "../../../account/client";
+import PeopleDetails from "./details";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function PeopleTable({ users = [], fetchUsers }: { users?: any[]; fetchUsers: () => void; }) {
+  const [showDetails, setShowDetails] = useState(false);
+  const [showUserId, setShowUserId] = useState<string | null>(null);
+  return (
+    <Table striped>
+      {showDetails && (
+        <PeopleDetails
+          uid={showUserId}
+          onClose={() => {
+            setShowDetails(false);
+            fetchUsers();
+          }}/>
+      )}
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Login ID</th>
+          <th>Section</th>
+          <th>Role</th>
+          <th>Last Activity</th>
+          <th>Total Activity</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user: any) => (
+          <tr key={user._id}>
+            <td className="wd-full-name text-nowrap">
+              <span className="text-decoration-none"
+                onClick={() => {
+                  setShowDetails(true);
+                  setShowUserId(user._id);
+              }} >
+                <FaUserCircle className="me-2 fs-1 text-secondary" />
+                <span className="wd-first-name">{user.firstName}</span>{" "}
+                <span className="wd-last-name">{user.lastName}</span>
+              </span>
+            </td>
+            <td className="wd-login-id">{user.loginId || user._id}</td>
+            <td className="wd-section">{user.section || "—"}</td>
+            <td className="wd-role">{user.role}</td>
+            <td className="wd-last-activity">
+              {user.lastActivity ? new Date(user.lastActivity).toLocaleDateString() : "—"}
+            </td>
+            <td className="wd-total-activity">{user.totalActivity || "—"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
