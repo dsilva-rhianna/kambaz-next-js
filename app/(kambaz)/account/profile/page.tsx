5ea49fcd56e084ea62685435118c0c0e30a66f1f
dsilva-rhianna <60183173+dsilva-rhianna@users.noreply.github.com>
2026-04-13 14:14:36 -0400

    testing

--- app/(kambaz)/account/profile/page.tsx
+++ app/(kambaz)/account/profile/page.tsx
@@ -14,18 +14,19 @@ export default function Profile() {
       const updatedProfile = await client.updateUser(profile);
       dispatch(setCurrentUser(updatedProfile));
     };
-  const fetchProfile = () => {
-    if (!currentUser) return redirect("/account/signin");
-    setProfile(currentUser);
-  };
   const signout = async () => {
     await client.signout();
     dispatch(setCurrentUser(null));
     redirect("/account/signin");
   };
   useEffect(() => {
-    fetchProfile();
-  }, []);
+    if (currentUser) {
+      setProfile(currentUser);
+    }
+  }, [currentUser]);
+  if (!currentUser) {
+    return <div>Loading...</div>;
+  }
   return (
    <div className="wd-profile-screen">
       <h3>Profile</h3>
